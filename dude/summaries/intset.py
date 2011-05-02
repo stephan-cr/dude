0847ea8c325c47317d77b4654ffe52e2b5dfb5c4
stephan <stephan.cr@gmx.de>
2011-05-02 23:25:23 +0200

    fixed intset summary, it serves as an example how to define summaries

--- dude/summaries/intset.py
+++ dude/summaries/intset.py
@@ -3,8 +3,13 @@
 # See accompanying file LICENSE
 
 import generic_summary
+
+def split_line(line):
+    return line.split(':')[1].split('(')[1].split(' ')[0]
+
 class Intset(generic_summary.LineSelect):
     def __init__(self, name, groupby):
-        generic_summary.LineSelect(name = name, groupby = groupby,
-                                   split = lambda line: line.split(':')[1].split('(')[1].split(' ')[0],
-                                   regex = '^#txs', header = 'txs')
+        generic_summary.LineSelect.__init__(self, name = name,
+                                            groupby = groupby,
+                                            split = split_line,
+                                            regex = '^#txs', header = 'txs')
