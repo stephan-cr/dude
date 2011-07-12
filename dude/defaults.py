26f5592f9645f95709d2f690ea94d1493b4a4d22
stephan_cr <stephan.cr@gmx.de>
2011-07-12 10:20:24 +0200

    simplified order function "factory"

--- dude/defaults.py
+++ dude/defaults.py
@@ -12,10 +12,10 @@ home = os.environ['HOME']
 def order_dim(dims):
     '''
     order function "factory": generates a compare function which simply orders
-    the experiments dimensions wise, for each dimension it uses the default
+    the experiments dimension wise, for each dimension it uses the default
     "cmp" function
     '''
-    def __order_dim_function_helper(dims, optpt1, optpt2):
+    def __order_dim_function_helper(optpt1, optpt2):
         for dim in dims:
             assert (dim in optpt1) and (dim in optpt2)
             c = cmp(optpt1[dim], optpt2[dim])
@@ -24,5 +24,4 @@ def order_dim(dims):
 
         return 0
 
-    return lambda optpt1, optpt2: \
-        __order_dim_function_helper(dims, optpt1, optpt2)
+    return __order_dim_function_helper
