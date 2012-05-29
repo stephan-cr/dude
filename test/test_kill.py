474440e8ad5b23429f9b2d09fbca480098d13e03
Diogo Behrens <db7@users.noreply.github.com>
2012-05-29 13:15:57 +0200

    test kill

--- test/test_kill.py
+++ test/test_kill.py
@@ -10,9 +10,12 @@ optpt = { 'hello' : 'world' }
 def kill_spawned():
     p = execute.SpawnProcess(foo, optpt, sys.stdout, sys.stdin)
     p.start()
+    #print "started"
     time.sleep(1)
     p.kill()
+    #print "killed"
     p.poll()
+    
         
 class CoreTestCase(unittest.TestCase):
     def test_kill_spawned(self):
