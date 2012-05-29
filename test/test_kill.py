b511f9c35cc4ea79095cd9a39a9aa13beed77023
Diogo Behrens <db7@users.noreply.github.com>
2012-05-29 13:09:40 +0200

    test kill

---
+++ test/test_kill.py
@@ -0,0 +1,22 @@
+import unittest
+import sys, os, signal, time
+import dude.execute as execute
+
+def foo(optpt):
+    return "echo"
+
+optpt = { 'hello' : 'world' }
+
+def kill_spawned():
+    p = execute.SpawnProcess(foo, optpt, sys.stdout, sys.stdin)
+    p.start()
+    time.sleep(1)
+    p.kill()
+    p.poll()
+        
+class CoreTestCase(unittest.TestCase):
+    def test_kill_spawned(self):
+        kill_spawned()
+        
+if __name__ == '__main__':
+    kill_spawned()
