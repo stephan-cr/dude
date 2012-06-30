bc27cab7dc0c65c7130117fe9daab5b75055dc83
Diogo Behrens <db7@users.noreply.github.com>
2012-06-30 13:15:48 +0200

    simple test cases for summaries

---
+++ test/summaries/test.py
@@ -0,0 +1,42 @@
+import sys, os
+sys.path.insert(0, os.path.abspath('../..'))
+import dude.summaries
+
+optpt = {'a': 1, 'b' : 'x'}
+
+s = dude.summaries.LineSelect("test", header = "bla_col")
+f = open('bla.txt')
+print s.header(optpt.keys())
+s.visit(optpt, f, sys.stdout)
+print "----------"
+
+s = dude.summaries.FilesLineSelect("test", files='bla.*', header = 'val')
+f = open('/dev/null')
+print s.header(optpt.keys())
+s.visit(optpt, f, sys.stdout)
+print "----------"
+
+s = dude.summaries.MultiLineSelect(
+    "test", 
+    filters = [
+        ('hey', '.*', (lambda l:l)),
+        ('ho', '.*', (lambda l:l))
+        ]
+    )
+f = open('bla.txt')
+print s.header(optpt.keys())
+s.visit(optpt, f, sys.stdout)
+print "----------"
+
+s = dude.summaries.FilesMultiLineSelect(
+    "test",
+    files  = "bla*",
+    filters = [
+        ('hey', '.*', (lambda l:l)),
+        ('ho', '.*', (lambda l:l))
+        ]
+    )
+f = open('/dev/null')
+print s.header(optpt.keys())
+s.visit(optpt, f, sys.stdout)
+print "----------"
